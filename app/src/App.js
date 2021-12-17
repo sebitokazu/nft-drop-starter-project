4680eae829a51c948753b3f821d7edc5c7212654
Seba Itokazu <sebitokazu@gmail.com>
2021-12-16 23:07:12 -0300

    Connect webapp to deployed candy machine and fetch metadata

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
+import CandyMachine from "./CandyMachine";
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
-
   const [walletAddress, setWalletAddress] = useState(null);
 
   const checkIfWalletIsConnected = async () => {
@@ -16,21 +16,21 @@ const App = () => {
 
       if (solana) {
         if (solana.isPhantom) {
-          console.log('Phantom wallet found!');
-          
+          console.log("Phantom wallet found!");
+
           /*
-          * The solana object gives us a function that will allow us to connect
-          * directly with the user's wallet!
-          */
+           * The solana object gives us a function that will allow us to connect
+           * directly with the user's wallet!
+           */
           const response = await solana.connect({ onlyIfTrusted: true });
           console.log(
-            'Connected with Public Key:',
+            "Connected with Public Key:",
             response.publicKey.toString()
           );
           setWalletAddress(response.publicKey.toString());
         }
       } else {
-        alert('Solana object not found! Get a Phantom Wallet 👻');
+        alert("Solana object not found! Get a Phantom Wallet 👻");
       }
     } catch (error) {
       console.error(error);
@@ -41,36 +41,36 @@ const App = () => {
    * When our component first mounts, let's check to see if we have a connected
    * Phantom Wallet
    */
-    useEffect(() => {
-      const onLoad = async () => {
-        await checkIfWalletIsConnected();
-      };
-      window.addEventListener('load', onLoad);
-      return () => window.removeEventListener('load', onLoad);
-    }, []);
-
-    const connectWallet = async () => {
-      const { solana } = window;
-
-      if (solana) {
-        const response = await solana.connect();
-        console.log('Connected with Public Key:', response.publicKey.toString());
-        setWalletAddress(response.publicKey.toString());
-      }
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
     };
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
 
-    /*
-     * We want to render this UI when the user hasn't connected
-     * their wallet to our app yet.
-     */
-    const renderNotConnectedContainer = () => (
-      <button
-        className="cta-button connect-wallet-button"
-        onClick={connectWallet}
-      >
-        Connect to Wallet
-      </button>
-    );
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with Public Key:", response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  /*
+   * We want to render this UI when the user hasn't connected
+   * their wallet to our app yet.
+   */
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
 
   return (
     <div className="App">
@@ -81,6 +81,9 @@ const App = () => {
           {/* Render your connect to wallet button right here */}
           {!walletAddress && renderNotConnectedContainer()}
         </div>
+        {/* Check for walletAddress and then pass in walletAddress */}
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
+
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
